32c15c1ae242431185892a09463d28ab7aa00d9c
U-PEROOT\URAJAUD <URAJAUD@LKSRIWL-29QR662.PEROOT.com>
2016-04-06 10:41:35 +0530

    File read eg

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var http = require("http");
+var fs = require("fs");
+var data = fs.readFileSync("input.txt");
 
 http.createServer(function (request, response) {
 
@@ -8,3 +10,7 @@ http.createServer(function (request, response) {
 }).listen(8081);
 
 console.log('Server running at http://127.0.0.1:8081/');
+
+
+console.log(data.toString());
+console.log("Program ended...");
